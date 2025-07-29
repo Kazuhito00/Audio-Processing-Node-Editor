b3bdf4f12d8fb7316362ad64c6f5cda7be4dc67c
KazuhitoTakahashi <shigeki.takahashi.dev@gmail.com>
2025-07-29 11:19:41 +0900

    Zeroノード追加。

---
+++ node/input_node/node_input_zero.py
@@ -0,0 +1,243 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import time
+from typing import Any, Dict, List, Optional
+
+import dearpygui.dearpygui as dpg  # type: ignore
+import numpy as np
+from node_editor.util import dpg_set_value, get_tag_name_list  # type: ignore
+
+from node.node_abc import DpgNodeABC  # type: ignore
+
+
+class Node(DpgNodeABC):
+    _ver: str = "0.0.1"
+
+    node_label: str = "Zero"
+    node_tag: str = "Zero"
+
+    def __init__(self) -> None:
+        self._node_data = {}
+        self._start_time: Optional[float] = None
+        self._paused_elapsed: float = 0.0
+
+    def add_node(
+        self,
+        parent: str,
+        node_id: int,
+        pos: List[int] = [0, 0],
+        setting_dict: Optional[Dict[str, Any]] = None,
+        callback: Optional[Any] = None,
+    ) -> str:
+        # タグ名
+        tag_name_list: List[Any] = get_tag_name_list(
+            node_id,
+            self.node_tag,
+            [],
+            [self.TYPE_SIGNAL_CHUNK, self.TYPE_TIME_MS],
+        )
+        tag_node_name: str = tag_name_list[0]
+        input_tag_list = tag_name_list[1]
+        output_tag_list = tag_name_list[2]
+
+        # 設定
+        self._setting_dict = setting_dict or {}
+        waveform_w: int = self._setting_dict.get("waveform_width", 200)
+        waveform_h: int = self._setting_dict.get("waveform_height", 400)
+        self._default_sampling_rate: int = self._setting_dict.get(
+            "default_sampling_rate", 16000
+        )
+        self._chunk_size: int = self._setting_dict.get("chunk_size", 1024)
+        self._use_pref_counter: bool = self._setting_dict["use_pref_counter"]
+
+        self._node_data[str(node_id)] = {
+            "zero_chunk": np.zeros(self._chunk_size, dtype=np.float32),
+            "chunk_index": -1,
+            "display_x_buffer": np.array([]),
+            "display_y_buffer": np.array([]),
+        }
+
+        # 表示用バッファ用意
+        buffer_len: int = self._default_sampling_rate * 5
+        self._node_data[str(node_id)]["display_y_buffer"] = np.zeros(
+            buffer_len, dtype=np.float32
+        )
+        self._node_data[str(node_id)]["display_x_buffer"] = (
+            np.arange(len(self._node_data[str(node_id)]["display_y_buffer"]))
+            / self._default_sampling_rate
+        )
+
+        # ノード
+        with dpg.node(
+            tag=tag_node_name,
+            parent=parent,
+            label=self.node_label,
+            pos=pos,
+        ):
+            # プロットエリア
+            with dpg.node_attribute(
+                tag=output_tag_list[0][0],
+                attribute_type=dpg.mvNode_Attr_Output,
+            ):
+                with dpg.plot(
+                    height=waveform_h,
+                    width=waveform_w,
+                    no_inputs=False,
+                    tag=f"{node_id}:audio_plot_area",
+                ):
+                    dpg.add_plot_axis(
+                        dpg.mvXAxis,
+                        label="Time(s)",
+                        no_label=True,
+                        no_tick_labels=True,
+                        tag=f"{node_id}:xaxis",
+                    )
+                    dpg.add_plot_axis(
+                        dpg.mvYAxis,
+                        label="Amplitude",
+                        no_label=True,
+                        no_tick_labels=True,
+                        tag=f"{node_id}:yaxis",
+                    )
+                    dpg.set_axis_limits(f"{node_id}:xaxis", 0.0, 5.0)
+                    dpg.set_axis_limits(f"{node_id}:yaxis", -1.0, 1.0)
+
+                    dpg.add_line_series(
+                        [],
+                        [],
+                        parent=f"{node_id}:yaxis",
+                        tag=f"{node_id}:audio_line_series",
+                    )
+            # 処理時間
+            if self._use_pref_counter:
+                with dpg.node_attribute(
+                    tag=output_tag_list[1][0],
+                    attribute_type=dpg.mvNode_Attr_Output,
+                ):
+                    dpg.add_text(
+                        tag=output_tag_list[1][1],
+                        default_value="elapsed time(ms)",
+                    )
+
+        return tag_node_name
+
+    def update(
+        self,
+        node_id: str,
+        connection_list: List[Any],
+        player_status_dict: Dict[str, Any],
+        node_result_dict: Dict[str, Any],
+    ) -> Any:
+        tag_name_list: List[Any] = get_tag_name_list(
+            node_id,
+            self.node_tag,
+            [],
+            [self.TYPE_SIGNAL_CHUNK, self.TYPE_TIME_MS],
+        )
+        output_tag_list = tag_name_list[2]
+
+        # 計測開始
+        if self._use_pref_counter:
+            start_time = time.perf_counter()
+
+        # 再生に合わせてスクロールし、チャンク設定を行う
+        chunk_index = 0
+        current_status = player_status_dict.get("current_status", False)
+        if current_status == "play":
+            if self._start_time is None:
+                self._start_time = time.time() - self._paused_elapsed
+
+            elapsed = time.time() - self._start_time
+            sr = self._default_sampling_rate
+            chunk_time = self._chunk_size / sr
+            chunk_index = int(elapsed / chunk_time)
+
+            # チャンクインデックスが更新されたらゼロチャンクを作成
+            if self._node_data[str(node_id)]["chunk_index"] < chunk_index:
+                # プロット更新
+                temp_display_y_buffer = self._node_data[str(node_id)][
+                    "display_y_buffer"
+                ]
+                temp_display_y_buffer = np.roll(
+                    temp_display_y_buffer, -self._chunk_size
+                )
+                temp_display_y_buffer[-self._chunk_size :] = self._node_data[
+                    str(node_id)
+                ]["zero_chunk"]
+                self._node_data[str(node_id)]["display_y_buffer"] = (
+                    temp_display_y_buffer
+                )
+                dpg.set_value(
+                    f"{node_id}:audio_line_series",
+                    [
+                        self._node_data[str(node_id)]["display_x_buffer"],
+                        temp_display_y_buffer,
+                    ],
+                )
+
+            # チャンクインデックス更新
+            self._node_data[str(node_id)]["chunk_index"] = chunk_index
+
+        elif current_status == "pause":
+            if self._start_time is not None:
+                self._paused_elapsed = time.time() - self._start_time
+                self._start_time = None
+
+        elif current_status == "stop":
+            self._start_time = None
+            self._paused_elapsed = 0.0
+            # チャンクインデックス初期化
+            self._node_data[str(node_id)]["chunk_index"] = -1
+
+            # プロットエリア初期化
+            buffer_len: int = self._default_sampling_rate * 5
+            self._node_data[str(node_id)]["display_y_buffer"] = np.zeros(
+                buffer_len, dtype=np.float32
+            )
+            self._node_data[str(node_id)]["display_x_buffer"] = (
+                np.arange(len(self._node_data[str(node_id)]["display_y_buffer"]))
+                / self._default_sampling_rate
+            )
+            dpg.set_value(
+                f"{node_id}:audio_line_series",
+                [
+                    self._node_data[str(node_id)]["display_x_buffer"].tolist(),
+                    list(self._node_data[str(node_id)]["display_y_buffer"]),
+                ],
+            )
+
+        result_dict = {
+            "chunk_index": self._node_data[str(node_id)]["chunk_index"],
+            "chunk": self._node_data[str(node_id)]["zero_chunk"],
+        }
+
+        # 計測終了
+        if self._use_pref_counter:
+            elapsed_time = time.perf_counter() - start_time
+            elapsed_time = int(elapsed_time * 1000)
+            dpg_set_value(output_tag_list[1][1], str(elapsed_time).zfill(4) + "ms")
+
+        return result_dict
+
+    def close(self, node_id: str) -> None:
+        pass
+
+    def get_setting_dict(self, node_id: str) -> Dict[str, Any]:
+        tag_name_list: List[Any] = get_tag_name_list(
+            node_id,
+            self.node_tag,
+            [],
+            [self.TYPE_SIGNAL_CHUNK, self.TYPE_TIME_MS],
+        )
+        tag_node_name: str = tag_name_list[0]
+
+        pos: List[int] = dpg.get_item_pos(tag_node_name)
+
+        setting_dict: Dict[str, Any] = {
+            "ver": self._ver,
+            "pos": pos,
+        }
+        return setting_dict
+
+    def set_setting_dict(self, node_id: int, setting_dict: Dict[str, Any]) -> None:
+        pass
